bf6e13c4836f25aa1bb14aba36f58ed14d5cfdd1
o-meneses-covarrubias <53591081+o-meneses-covarrubias@users.noreply.github.com>
2019-11-20 13:24:56 -0600

    Update example1.R

--- demo/example1.R
+++ demo/example1.R
@@ -5,8 +5,16 @@ library(Rtauchen)
 # lambda = 0.95
 # m = 3
 results1 = Rtauchen(5, 0.02, 0.98, 3)
+
+#Ejemplo Univariado:
+# A=0.98
+# Ni=5
+# SSigma_eps=0.02^2
+# m=3
 results2 = RtauchenVAR(5, 0.02^2, 0.98, 3)
+
 print(results1-results2)
+
 
 #Ejemplo Multivariado:
 A=matrix(c(0.7,0.3,0.2,0.5),ncol=2)
@@ -14,24 +22,24 @@ Ni=c(3,5)
 SSigma_eps=diag(2)*(0.02^2)
 m=3
 
-#Muestra la lista con la grid para cada dimensi髇
+#Muestra la lista con la grid para cada dimensi贸n
 grid = TgridVAR(Ni, SSigma_eps, A, m)
 print(grid)
 for (i in 1:length(Ni)) {
   print(grid[[i]])
 }
-#Muestra la matriz de transi髇 de los n1*n2*...*nI estados y muestra la suma horizontal
+#Muestra la matriz de transi贸n de los n1*n2*...*nI estados y muestra la suma horizontal
 results3 = RtauchenVAR(Ni, SSigma_eps, A, m)
 print(results3)
 for (i in 1:prod(Ni)) {
   print(sum(results3[i,]))
 }
 
-#Muestra los valores de cada estado y cada dimensi髇
+#Muestra los valores de cada estado y cada dimensi贸n
 statesgrid = Tstates_grid(Ni, SSigma_eps, A, m)
 print(statesgrid)
 
-##Muestra los valores de cada estado y cada dimensi髇 con respecto al indice que ocupan en la
+##Muestra los valores de cada estado y cada dimensi贸n con respecto al indice que ocupan en la
 #grid generada por TgridVAR
 statesgridindex = Tstates_grid_index(Ni)
 print(statesgridindex)
